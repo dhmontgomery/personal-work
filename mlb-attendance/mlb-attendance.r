c298bcf98892f3f835314a3e52c3e72f4ccee9b7
David Montgomery <dmontgomery@americanpublicmedia.org>
2019-04-17 11:13:14 -0500

    Modernize code

--- mlb-attendance/mlb-attendance.r
+++ mlb-attendance/mlb-attendance.r
@@ -1,91 +1,67 @@
 # Load libraries
 library(rvest)
-library(ggplot2)
+library(tidyverse)
 library(ggrepel)
-library(dplyr)
 library(scales)
+library(stringr)
 library(grid)
 library(lubridate)
 source("http://peterhaschke.com/Code/multiplot.R") # Load a function to plot multiple ggplot charts
 
-# Download attendance data
-url <- "http://www.espn.com/mlb/attendance" # Set the URL of the page
-attendance <- url %>% 
-  read_html() %>%
-  html_nodes(xpath='//*[@id="my-teams-table"]/div/div/table') %>% # Select the table
-  html_table()
-attendance <- attendance[[1]] # Extract the data frame from the text
-
-colnames(attendance) <- c("Rank2016","Team","HomeGames","HomeTotal","HomeAverage","HomePct","RoadGames","RoadAverage","RoadPct","OverallGames","OverallAverage","OverallPct") # Label the columns
-attendance <- attendance[-c(1:2),] # Drop the now-redundant first two rows
-rownames(attendance) <- 1:nrow(attendance) # Renumber the labels
-
 # Download ballpark capacity
 url <- "https://en.wikipedia.org/wiki/List_of_Major_League_Baseball_stadiums"
 capacity <- url %>%
-  read_html() %>%
-  html_nodes(xpath='//*[@id="mw-content-text"]/table[2]') %>%
-  html_table()
-capacity <- capacity[[1]]
-# Remove some footnoted brackets
-capacity[,3] <- gsub("\\[[^\\]]*\\]", "",capacity[,3], perl=TRUE)
-capacity[,6] <- gsub("\\[[^\\]]*\\]", "",capacity[,6], perl=TRUE)
+	read_html() %>%
+	html_nodes(xpath='//*[@id="mw-content-text"]/div/table[2]') %>%
+	html_table() %>%
+	`[[`(1) %>%
+	mutate_at(vars(3, 6), ~str_remove_all(., "\\[[^\\]]*\\]")) %>%
+	mutate_at(vars(3), ~str_remove_all(., ",") %>% as.numeric()) %>%
+	arrange(Team)
 
-# Sort the two tables to get them in the same order
-capacity <- capacity %>% arrange(Team)
-attendance <- attendance %>% arrange(Team)
-
-attendance$Capacity <- capacity[,3] # Copy the capacity column into the attendance table
-# Delete commas from numbers in the tables, to make them numbers instead of strings
-for (i in c(4,5,8,11,13)) { attendance[,i] <- as.numeric(gsub(",","",attendance[,i])) }
-attendance$HomePct <- attendance$HomeAverage / attendance$Capacity # Calculate home attendance as a percentage of capacity
-
-# Add three-letter labels for each team, for the plotting.
-attendance$Abbrev <- c("ARI","ATL","BAL","BOS","CHC","CHW","CIN","CLE","COL","DET","HOU","KCR","LAA","LAD","MIA","MIL","MIN","NYM","NYY","OAK","PHI","PIT","SDP","SFG","SEA","STL","TBR","TEX","TOR","WSN") 
+# Download attendance data
+url <- "http://www.espn.com/mlb/attendance" # Set the URL of the page
+attendance <- url %>% 
+	read_html() %>%
+	html_nodes(xpath='//*[@id="my-teams-table"]/div/div/table') %>% # Select the table
+	html_table() %>%
+	`[[`(1) %>%
+	set_names("Rank2016","Team","HomeGames","HomeTotal","HomeAverage","HomePct","RoadGames","RoadAverage","RoadPct","OverallGames","OverallAverage","OverallPct") %>% # Label the columns
+	slice(-c(1:2)) %>%
+	mutate_at(vars(4, 5, 8, 11), ~str_remove_all(., ",")) %>%
+	mutate_at(vars(1, 3:12), as.numeric) %>%
+	arrange(Team) %>%
+	bind_cols(capacity %>% select(Capacity)) %>%
+	mutate(HomePct = HomeAverage / Capacity,
+		   Abbrev = c("ARI","ATL","BAL","BOS","CHC","CHW","CIN","CLE","COL","DET","HOU","KCR","LAA","LAD","MIA","MIL","MIN","NYM","NYY","OAK","PHI","PIT","SDP","SFG","SEA","STL","TBR","TEX","TOR","WSN")) # Add three-letter labels for each team, for the plotting
 
 a <- ggplot(attendance, aes(HomeAverage,HomePct)) + # Make the first graph, attendance vs. percent attendance
     geom_point(size=1) + # Tiny points
     geom_text_repel( # This plots the labels, but around the points.
     	aes(label=Abbrev), 
     	size = 3) + # Set the font size
-		
-    ggtitle(paste0( # Title the plot dynamically
-    	year(Sys.Date()), # The current year
-    	" MLB home attendance vs. capacity (through ",
-    	strftime(Sys.Date()-1, format = "%m/%d"), # Yesterday
-    	")")) +
-    scale_x_continuous(
-        name="", # No x-axis title
-        labels=comma, # Insert commas into the x-axis labels
-        breaks=c(15000,25000,35000,45000,55000)) + # Set the breaks
-    scale_y_continuous(
-        name="Attendance as percent of capacity",
-        labels=percent) # Convert the y-axis into percentages.
+	scale_x_continuous(labels=comma, # Insert commas into the x-axis labels
+					   breaks=c(15000,25000,35000,45000,55000)) + # Set the breaks
+	scale_y_continuous(labels=percent_format(accuracy = 1)) + # Convert the y-axis into percentages.
+	labs(title = paste0( # Title the plot dynamically
+		year(Sys.Date()), # The current year
+		" MLB home attendance vs. capacity (through ",
+		strftime(Sys.Date()-1, format = "%m/%d"), # Yesterday
+		")"),
+		y = "Attendance as percent of capacity") +
+	theme(axis.title.x = element_blank())
+    	
 
 b <- ggplot(attendance, aes(HomeAverage,Capacity)) + # Make the second graph, attendance vs. capacity
     geom_point(size=1) + # Tiny points
-    geom_text_repel( #Plot labels
-    	aes(label=Abbrev), 
-    	size = 3) + #Set the font size
-    scale_x_continuous(
-        name="Average home attendance",
-        labels=comma, 
-        breaks=c(15000,25000,35000,45000,55000)) +
-    scale_y_continuous(
-        name="Ballpark capacity", 
-        labels=comma,
-        limits=c(30000,60000)) +
-    annotate( # Add a byline
-        geom="text", 
-        label="By David Montgomery, github.com/dhmontgomery",
-        x=max(attendance$HomeAverage), # Position the byline in the lower right corner
-        y=min(attendance$Capacity),
-        size=2.5, 
-        hjust=1) # Justify right
+    geom_text_repel(aes(label=Abbrev), size = 3) + #Set the font size
+    scale_x_continuous(labels=comma, breaks=c(15000,25000,35000,45000,55000)) +
+	scale_y_continuous(labels=comma_format(scale = 0.001, suffix = "K"), limits=c(20000,60000)) +
+	labs(caption = "By David Montgomery, github.com/dhmontgomery",
+		 x = "Average home attendance",
+		 y = "Ballpark capacity")
 
 # Save as a square PNG
-try( # Suppress an apparently harmless error message
-	ggsave("attendance.png",plot=multiplot(a,b), device="png", height=10, width=10),
-	silent = T)
+ggsave("attendance.png",plot=multiplot(a,b), device="png", height=10, width=10)
 	
-attendance[,c(2:6)] %>% write.csv("mlb-attendance.csv")
+attendance %>% select(2:6) %>% write_csv("mlb-attendance.csv")
